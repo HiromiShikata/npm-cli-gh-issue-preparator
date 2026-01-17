c3dcc5b2128779b02ca1ae17737c79c1b568e549
gh-actions <gh-actions>
2026-01-17 01:15:34 +0000

    autogen: release

--- types/domain/usecases/StartPreparationUseCase.d.ts
+++ types/domain/usecases/StartPreparationUseCase.d.ts
@@ -2,21 +2,17 @@ import { IssueRepository } from './adapter-interfaces/IssueRepository';
 import { ProjectRepository } from './adapter-interfaces/ProjectRepository';
 import { LocalCommandRunner } from './adapter-interfaces/LocalCommandRunner';
 export declare class StartPreparationUseCase {
-  private readonly projectRepository;
-  private readonly issueRepository;
-  private readonly localCommandRunner;
-  maximumPreparingIssuesCount: number;
-  constructor(
-    projectRepository: ProjectRepository,
-    issueRepository: IssueRepository,
-    localCommandRunner: LocalCommandRunner,
-  );
-  run: (params: {
-    projectUrl: string;
-    awaitingWorkspaceStatus: string;
-    preparationStatus: string;
-    defaultAgentName: string;
-    logFilePath?: string;
-  }) => Promise<void>;
+    private readonly projectRepository;
+    private readonly issueRepository;
+    private readonly localCommandRunner;
+    maximumPreparingIssuesCount: number;
+    constructor(projectRepository: ProjectRepository, issueRepository: IssueRepository, localCommandRunner: LocalCommandRunner);
+    run: (params: {
+        projectUrl: string;
+        awaitingWorkspaceStatus: string;
+        preparationStatus: string;
+        defaultAgentName: string;
+        logFilePath?: string;
+    }) => Promise<void>;
 }
 //# sourceMappingURL=StartPreparationUseCase.d.ts.map
