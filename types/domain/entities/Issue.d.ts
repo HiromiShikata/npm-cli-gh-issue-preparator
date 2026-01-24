cd677f46e8f25ed43de67f235c1067c63f900dcb
gh-actions <gh-actions>
2026-01-24 01:46:54 +0000

    autogen: release

--- types/domain/entities/Issue.d.ts
+++ types/domain/entities/Issue.d.ts
@@ -1,8 +1,8 @@
 export type Issue = {
-  id: string;
-  url: string;
-  title: string;
-  labels: string[];
-  status: string;
+    id: string;
+    url: string;
+    title: string;
+    labels: string[];
+    status: string;
 };
 //# sourceMappingURL=Issue.d.ts.map
