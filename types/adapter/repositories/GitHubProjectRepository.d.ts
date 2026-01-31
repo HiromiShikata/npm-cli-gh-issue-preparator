2a1c13863294f9d94bd68e25c7986936b40fb093
gh-actions <gh-actions>
2026-01-31 00:26:34 +0000

    autogen: release

--- types/adapter/repositories/GitHubProjectRepository.d.ts
+++ types/adapter/repositories/GitHubProjectRepository.d.ts
@@ -1,10 +1,10 @@
 import { ProjectRepository } from '../../domain/usecases/adapter-interfaces/ProjectRepository';
 import { Project } from '../../domain/entities/Project';
 export declare class GitHubProjectRepository implements ProjectRepository {
-  private readonly token;
-  constructor(token: string);
-  private parseGitHubProjectUrl;
-  getByUrl(url: string): Promise<Project>;
-  prepareStatus(name: string, project: Project): Promise<Project>;
+    private readonly token;
+    constructor(token: string);
+    private parseGitHubProjectUrl;
+    getByUrl(url: string): Promise<Project>;
+    prepareStatus(name: string, project: Project): Promise<Project>;
 }
 //# sourceMappingURL=GitHubProjectRepository.d.ts.map
