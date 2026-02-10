9c6257d3482dc83d0a733af167e2d55b4d52a63f
HiromiShikata <6440811+HiromiShikata@users.noreply.github.com>
2026-02-10 15:27:51 +0000

    style: `npm run fmt`

--- types/adapter/repositories/GitHubIssueRepository.d.ts
+++ types/adapter/repositories/GitHubIssueRepository.d.ts
@@ -1,17 +1,20 @@
-import { IssueRepository, RelatedPullRequest } from '../../domain/usecases/adapter-interfaces/IssueRepository';
+import {
+  IssueRepository,
+  RelatedPullRequest,
+} from '../../domain/usecases/adapter-interfaces/IssueRepository';
 import { Issue } from '../../domain/entities/Issue';
 import { Project } from '../../domain/entities/Project';
 export declare class GitHubIssueRepository implements IssueRepository {
-    private readonly token;
-    constructor(token: string);
-    private parseProjectInfo;
-    private buildProjectItemsQuery;
-    private mapCommentsToEntity;
-    private getStatusOptionId;
-    getAllOpened(project: Project): Promise<Issue[]>;
-    update(issue: Issue, project: Project): Promise<void>;
-    get(issueUrl: string, project: Project): Promise<Issue | null>;
-    private parseIssueUrl;
-    findRelatedOpenPRs(issueUrl: string): Promise<RelatedPullRequest[]>;
+  private readonly token;
+  constructor(token: string);
+  private parseProjectInfo;
+  private buildProjectItemsQuery;
+  private mapCommentsToEntity;
+  private getStatusOptionId;
+  getAllOpened(project: Project): Promise<Issue[]>;
+  update(issue: Issue, project: Project): Promise<void>;
+  get(issueUrl: string, project: Project): Promise<Issue | null>;
+  private parseIssueUrl;
+  findRelatedOpenPRs(issueUrl: string): Promise<RelatedPullRequest[]>;
 }
 //# sourceMappingURL=GitHubIssueRepository.d.ts.map
