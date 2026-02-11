c151c2d3ed7a324f86a1c7e025aa83255cbf7ad5
gh-actions <gh-actions>
2026-02-11 11:24:12 +0000

    autogen: release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+# [1.9.0](https://github.com/HiromiShikata/npm-cli-gh-issue-preparator/compare/v1.8.0...v1.9.0) (2026-02-11)
+
+
+### Features
+
+* **core:** use getStoryObjectMap from tower-defence library ([980aed8](https://github.com/HiromiShikata/npm-cli-gh-issue-preparator/commit/980aed86127965c68d38e1336ef007aad5d810ee)), closes [#82](https://github.com/HiromiShikata/npm-cli-gh-issue-preparator/issues/82)
+
 # [1.8.0](https://github.com/HiromiShikata/npm-cli-gh-issue-preparator/compare/v1.7.1...v1.8.0) (2026-02-09)
 
 
--- types/adapter/repositories/TowerDefenceIssueRepository.d.ts
+++ types/adapter/repositories/TowerDefenceIssueRepository.d.ts
@@ -1,23 +1,20 @@
-import {
-  IssueRepository,
-  RelatedPullRequest,
-} from '../../domain/usecases/adapter-interfaces/IssueRepository';
+import { IssueRepository, RelatedPullRequest } from '../../domain/usecases/adapter-interfaces/IssueRepository';
 import { Issue } from '../../domain/entities/Issue';
 import { Project } from '../../domain/entities/Project';
 export declare class TowerDefenceIssueRepository implements IssueRepository {
-  private readonly configFilePath;
-  private readonly token;
-  private cachedProject;
-  private cachedIssues;
-  constructor(configFilePath: string, token: string);
-  private parseProjectUrl;
-  private loadData;
-  getAllOpened(project: Project): Promise<Issue[]>;
-  get(issueUrl: string, project: Project): Promise<Issue | null>;
-  private mapToIssue;
-  private getStatusOptionId;
-  update(issue: Issue, project: Project): Promise<void>;
-  private parseIssueUrl;
-  findRelatedOpenPRs(issueUrl: string): Promise<RelatedPullRequest[]>;
+    private readonly configFilePath;
+    private readonly token;
+    private cachedProject;
+    private cachedIssues;
+    constructor(configFilePath: string, token: string);
+    private parseProjectUrl;
+    private loadData;
+    getAllOpened(project: Project): Promise<Issue[]>;
+    get(issueUrl: string, project: Project): Promise<Issue | null>;
+    private mapToIssue;
+    private getStatusOptionId;
+    update(issue: Issue, project: Project): Promise<void>;
+    private parseIssueUrl;
+    findRelatedOpenPRs(issueUrl: string): Promise<RelatedPullRequest[]>;
 }
 //# sourceMappingURL=TowerDefenceIssueRepository.d.ts.map
