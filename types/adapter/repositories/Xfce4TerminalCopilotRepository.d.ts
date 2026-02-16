fd200ac8a9aee09cacb1c5c8071a35bdfbaff22f
gh-actions <gh-actions>
2026-02-16 01:42:17 +0000

    autogen: release

--- types/adapter/repositories/Xfce4TerminalCopilotRepository.d.ts
+++ types/adapter/repositories/Xfce4TerminalCopilotRepository.d.ts
@@ -1,6 +1,6 @@
 import { CopilotRepository } from '../../domain/usecases/adapter-interfaces/CopilotRepository';
 export declare class Xfce4TerminalCopilotRepository implements CopilotRepository {
-  private escapeForSingleQuotes;
-  run(prompt: string, model: 'gpt-5-mini', processTitle: string): void;
+    private escapeForSingleQuotes;
+    run(prompt: string, model: 'gpt-5-mini', processTitle: string): void;
 }
 //# sourceMappingURL=Xfce4TerminalCopilotRepository.d.ts.map
