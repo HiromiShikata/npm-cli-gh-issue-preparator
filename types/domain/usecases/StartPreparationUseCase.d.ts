b5a52c8b88d96e0a381c9685eaf3728c0c0d3059
gh-actions <gh-actions>
2026-02-21 01:07:01 +0000

    autogen: release

--- types/domain/usecases/StartPreparationUseCase.d.ts
+++ types/domain/usecases/StartPreparationUseCase.d.ts
@@ -4,30 +4,22 @@ import { LocalCommandRunner } from './adapter-interfaces/LocalCommandRunner';
 import { StoryObjectMap } from '../entities/StoryObjectMap';
 import { ClaudeRepository } from './adapter-interfaces/ClaudeRepository';
 export declare class StartPreparationUseCase {
-  private readonly projectRepository;
-  private readonly issueRepository;
-  private readonly claudeRepository;
-  private readonly localCommandRunner;
-  constructor(
-    projectRepository: Pick<ProjectRepository, 'getByUrl'>,
-    issueRepository: Pick<
-      IssueRepository,
-      'getAllOpened' | 'getStoryObjectMap' | 'update'
-    >,
-    claudeRepository: Pick<ClaudeRepository, 'getUsage'>,
-    localCommandRunner: LocalCommandRunner,
-  );
-  run: (params: {
-    projectUrl: string;
-    awaitingWorkspaceStatus: string;
-    preparationStatus: string;
-    defaultAgentName: string;
-    logFilePath?: string;
-    maximumPreparingIssuesCount: number | null;
-  }) => Promise<void>;
-  createWorkflowBockerIsues: (storyObjectMap: StoryObjectMap) => {
-    orgRepo: string;
-    blockerIssueUrls: string[];
-  }[];
+    private readonly projectRepository;
+    private readonly issueRepository;
+    private readonly claudeRepository;
+    private readonly localCommandRunner;
+    constructor(projectRepository: Pick<ProjectRepository, 'getByUrl'>, issueRepository: Pick<IssueRepository, 'getAllOpened' | 'getStoryObjectMap' | 'update'>, claudeRepository: Pick<ClaudeRepository, 'getUsage'>, localCommandRunner: LocalCommandRunner);
+    run: (params: {
+        projectUrl: string;
+        awaitingWorkspaceStatus: string;
+        preparationStatus: string;
+        defaultAgentName: string;
+        logFilePath?: string;
+        maximumPreparingIssuesCount: number | null;
+    }) => Promise<void>;
+    createWorkflowBockerIsues: (storyObjectMap: StoryObjectMap) => {
+        orgRepo: string;
+        blockerIssueUrls: string[];
+    }[];
 }
 //# sourceMappingURL=StartPreparationUseCase.d.ts.map
