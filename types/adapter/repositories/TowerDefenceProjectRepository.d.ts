0e6de4201978289e87e38b97a6727bec56e9ba67
gh-actions <gh-actions>
2026-02-28 00:45:50 +0000

    autogen: release

--- types/adapter/repositories/TowerDefenceProjectRepository.d.ts
+++ types/adapter/repositories/TowerDefenceProjectRepository.d.ts
@@ -1,14 +1,14 @@
 import { ProjectRepository } from '../../domain/usecases/adapter-interfaces/ProjectRepository';
 import { Project } from '../../domain/entities/Project';
 export declare class TowerDefenceProjectRepository implements ProjectRepository {
-  private readonly configFilePath;
-  private readonly _token;
-  private cachedProject;
-  private projectUrl;
-  constructor(configFilePath: string, _token: string);
-  private loadProject;
-  getByUrl(url: string): Promise<Project>;
-  prepareStatus(_name: string, project: Project): Promise<Project>;
-  private mapToProject;
+    private readonly configFilePath;
+    private readonly _token;
+    private cachedProject;
+    private projectUrl;
+    constructor(configFilePath: string, _token: string);
+    private loadProject;
+    getByUrl(url: string): Promise<Project>;
+    prepareStatus(_name: string, project: Project): Promise<Project>;
+    private mapToProject;
 }
 //# sourceMappingURL=TowerDefenceProjectRepository.d.ts.map
