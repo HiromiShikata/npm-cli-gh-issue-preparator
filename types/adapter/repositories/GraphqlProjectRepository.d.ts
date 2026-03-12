88457fb1bcbb301b5ccce145f400eebec5cc7ece
HiromiShikata <6440811+HiromiShikata@users.noreply.github.com>
2026-03-12 14:25:31 +0000

    style: `npm run fmt`

--- types/adapter/repositories/GraphqlProjectRepository.d.ts
+++ types/adapter/repositories/GraphqlProjectRepository.d.ts
@@ -1,7 +1,7 @@
 export declare class GraphqlProjectRepository {
-    private readonly token;
-    constructor(token: string);
-    fetchReadme(projectUrl: string): Promise<string | null>;
-    private parseProjectUrl;
+  private readonly token;
+  constructor(token: string);
+  fetchReadme(projectUrl: string): Promise<string | null>;
+  private parseProjectUrl;
 }
 //# sourceMappingURL=GraphqlProjectRepository.d.ts.map
