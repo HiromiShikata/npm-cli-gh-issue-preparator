9b812fc78c98f22b6a53574e385cb00214fcb53c
HiromiShikata <6440811+HiromiShikata@users.noreply.github.com>
2026-03-21 04:26:30 +0000

    style: `npm run fmt`

--- types/domain/entities/Project.d.ts
+++ types/domain/entities/Project.d.ts
@@ -1,10 +1,10 @@
 export type Project = {
-    id: string;
-    url: string;
-    name: string;
-    statuses: string[];
-    customFieldNames: string[];
-    statusFieldId: string | null;
-    readme: string | null;
+  id: string;
+  url: string;
+  name: string;
+  statuses: string[];
+  customFieldNames: string[];
+  statusFieldId: string | null;
+  readme: string | null;
 };
 //# sourceMappingURL=Project.d.ts.map
--- types/domain/usecases/StartPreparationUseCase.d.ts
+++ types/domain/usecases/StartPreparationUseCase.d.ts
@@ -2,18 +2,22 @@ import { IssueRepository } from './adapter-interfaces/IssueRepository';
 import { ProjectRepository } from './adapter-interfaces/ProjectRepository';
 import { LocalCommandRunner } from './adapter-interfaces/LocalCommandRunner';
 export declare class StartPreparationUseCase {
-    private readonly projectRepository;
-    private readonly issueRepository;
-    private readonly localCommandRunner;
-    constructor(projectRepository: ProjectRepository, issueRepository: IssueRepository, localCommandRunner: LocalCommandRunner);
-    private parseMaximumPreparingIssuesCountFromReadme;
-    run: (params: {
-        projectUrl: string;
-        awaitingWorkspaceStatus: string;
-        preparationStatus: string;
-        defaultAgentName: string;
-        logFilePath?: string;
-        maximumPreparingIssuesCount: number | null;
-    }) => Promise<void>;
+  private readonly projectRepository;
+  private readonly issueRepository;
+  private readonly localCommandRunner;
+  constructor(
+    projectRepository: ProjectRepository,
+    issueRepository: IssueRepository,
+    localCommandRunner: LocalCommandRunner,
+  );
+  private parseMaximumPreparingIssuesCountFromReadme;
+  run: (params: {
+    projectUrl: string;
+    awaitingWorkspaceStatus: string;
+    preparationStatus: string;
+    defaultAgentName: string;
+    logFilePath?: string;
+    maximumPreparingIssuesCount: number | null;
+  }) => Promise<void>;
 }
 //# sourceMappingURL=StartPreparationUseCase.d.ts.map
