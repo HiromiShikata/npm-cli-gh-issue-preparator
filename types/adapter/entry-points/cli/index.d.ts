313ab7bc0d5764b40d1ab9e541d45157f93d3824
gh-actions <gh-actions>
2026-04-18 02:06:55 +0000

    autogen: release

--- types/adapter/entry-points/cli/index.d.ts
+++ types/adapter/entry-points/cli/index.d.ts
@@ -1,27 +1,23 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
 type ConfigFile = {
-  projectUrl?: string;
-  awaitingWorkspaceStatus?: string;
-  preparationStatus?: string;
-  defaultAgentName?: string;
-  defaultLlmModelName?: string;
-  defaultLlmAgentName?: string;
-  logFilePath?: string;
-  maximumPreparingIssuesCount?: number;
-  utilizationPercentageThreshold?: number;
-  allowedIssueAuthors?: string;
-  awaitingQualityCheckStatus?: string;
-  thresholdForAutoReject?: number;
-  workflowBlockerResolvedWebhookUrl?: string;
+    projectUrl?: string;
+    awaitingWorkspaceStatus?: string;
+    preparationStatus?: string;
+    defaultAgentName?: string;
+    defaultLlmModelName?: string;
+    defaultLlmAgentName?: string;
+    logFilePath?: string;
+    maximumPreparingIssuesCount?: number;
+    utilizationPercentageThreshold?: number;
+    allowedIssueAuthors?: string;
+    awaitingQualityCheckStatus?: string;
+    thresholdForAutoReject?: number;
+    workflowBlockerResolvedWebhookUrl?: string;
 };
 declare const loadConfigFile: (configFilePath: string) => ConfigFile;
 declare const parseProjectReadmeConfig: (readme: string) => ConfigFile;
-declare const mergeConfigs: (
-  configFile: ConfigFile,
-  cliOverrides: ConfigFile,
-  readmeOverrides: ConfigFile,
-) => ConfigFile;
+declare const mergeConfigs: (configFile: ConfigFile, cliOverrides: ConfigFile, readmeOverrides: ConfigFile) => ConfigFile;
 declare const program: Command;
 export { program, loadConfigFile, parseProjectReadmeConfig, mergeConfigs };
 //# sourceMappingURL=index.d.ts.map
