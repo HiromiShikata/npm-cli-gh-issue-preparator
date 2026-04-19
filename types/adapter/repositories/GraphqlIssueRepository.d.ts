ae7c2734f1269b676ca70ac9b5a85c2c90626499
HiromiShikata <6440811+HiromiShikata@users.noreply.github.com>
2026-04-19 07:41:48 +0000

    style: `npm run fmt`

--- types/adapter/repositories/GraphqlIssueRepository.d.ts
+++ types/adapter/repositories/GraphqlIssueRepository.d.ts
@@ -1,16 +1,22 @@
-import { IssueRepository, RelatedPullRequest } from '../../domain/usecases/adapter-interfaces/IssueRepository';
+import {
+  IssueRepository,
+  RelatedPullRequest,
+} from '../../domain/usecases/adapter-interfaces/IssueRepository';
 import { Issue } from '../../domain/entities/Issue';
 import { Project } from '../../domain/entities/Project';
-export declare class GraphqlIssueRepository implements Pick<IssueRepository, 'get' | 'update' | 'findRelatedOpenPRs' | 'getOpenPullRequest'> {
-    private readonly token;
-    constructor(token: string);
-    get(issueUrl: string, project: Project): Promise<Issue | null>;
-    private parseProjectUrl;
-    private getStatusOptionId;
-    update(issue: Issue, project: Project): Promise<void>;
-    private computePrStatus;
-    private parseIssueUrl;
-    findRelatedOpenPRs(issueUrl: string): Promise<RelatedPullRequest[]>;
-    getOpenPullRequest(prUrl: string): Promise<RelatedPullRequest | null>;
+export declare class GraphqlIssueRepository implements Pick<
+  IssueRepository,
+  'get' | 'update' | 'findRelatedOpenPRs' | 'getOpenPullRequest'
+> {
+  private readonly token;
+  constructor(token: string);
+  get(issueUrl: string, project: Project): Promise<Issue | null>;
+  private parseProjectUrl;
+  private getStatusOptionId;
+  update(issue: Issue, project: Project): Promise<void>;
+  private computePrStatus;
+  private parseIssueUrl;
+  findRelatedOpenPRs(issueUrl: string): Promise<RelatedPullRequest[]>;
+  getOpenPullRequest(prUrl: string): Promise<RelatedPullRequest | null>;
 }
 //# sourceMappingURL=GraphqlIssueRepository.d.ts.map
