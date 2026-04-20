abc9ed661cf87149118a2b0a9fb914f98737ee00
HiromiShikata <6440811+HiromiShikata@users.noreply.github.com>
2026-04-20 23:21:25 +0000

    style: `npm run fmt`

--- types/domain/usecases/StartPreparationUseCase.d.ts
+++ types/domain/usecases/StartPreparationUseCase.d.ts
@@ -3,22 +3,34 @@ import { ProjectRepository } from './adapter-interfaces/ProjectRepository';
 import { LocalCommandRunner } from './adapter-interfaces/LocalCommandRunner';
 import { ClaudeRepository } from './adapter-interfaces/ClaudeRepository';
 export declare class StartPreparationUseCase {
-    private readonly projectRepository;
-    private readonly issueRepository;
-    private readonly claudeRepository;
-    private readonly localCommandRunner;
-    constructor(projectRepository: Pick<ProjectRepository, 'getByUrl' | 'prepareStatus'>, issueRepository: Pick<IssueRepository, 'getAllOpened' | 'getStoryObjectMap' | 'update' | 'findRelatedOpenPRs' | 'getOpenPullRequest'>, claudeRepository: Pick<ClaudeRepository, 'isClaudeAvailable'>, localCommandRunner: LocalCommandRunner);
-    run: (params: {
-        projectUrl: string;
-        awaitingWorkspaceStatus: string;
-        preparationStatus: string;
-        defaultAgentName: string;
-        defaultLlmModelName: string | null;
-        defaultLlmAgentName: string | null;
-        logFilePath: string | null;
-        maximumPreparingIssuesCount: number | null;
-        utilizationPercentageThreshold: number;
-        allowedIssueAuthors: string[] | null;
-    }) => Promise<void>;
+  private readonly projectRepository;
+  private readonly issueRepository;
+  private readonly claudeRepository;
+  private readonly localCommandRunner;
+  constructor(
+    projectRepository: Pick<ProjectRepository, 'getByUrl' | 'prepareStatus'>,
+    issueRepository: Pick<
+      IssueRepository,
+      | 'getAllOpened'
+      | 'getStoryObjectMap'
+      | 'update'
+      | 'findRelatedOpenPRs'
+      | 'getOpenPullRequest'
+    >,
+    claudeRepository: Pick<ClaudeRepository, 'isClaudeAvailable'>,
+    localCommandRunner: LocalCommandRunner,
+  );
+  run: (params: {
+    projectUrl: string;
+    awaitingWorkspaceStatus: string;
+    preparationStatus: string;
+    defaultAgentName: string;
+    defaultLlmModelName: string | null;
+    defaultLlmAgentName: string | null;
+    logFilePath: string | null;
+    maximumPreparingIssuesCount: number | null;
+    utilizationPercentageThreshold: number;
+    allowedIssueAuthors: string[] | null;
+  }) => Promise<void>;
 }
 //# sourceMappingURL=StartPreparationUseCase.d.ts.map
