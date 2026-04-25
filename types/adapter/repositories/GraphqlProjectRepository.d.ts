1dc7d4448cf1966866e86d340c6b86d0b56936f4
gh-actions <gh-actions>
2026-04-25 13:03:43 +0000

    autogen: release

--- types/adapter/repositories/GraphqlProjectRepository.d.ts
+++ types/adapter/repositories/GraphqlProjectRepository.d.ts
@@ -1,13 +1,9 @@
 export declare class GraphqlProjectRepository {
-  private readonly token;
-  private readonly retryDelaysMs;
-  private readonly sleep;
-  constructor(
-    token: string,
-    retryDelaysMs?: number[],
-    sleep?: (ms: number) => Promise<void>,
-  );
-  fetchReadme(projectUrl: string): Promise<string | null>;
-  private parseProjectUrl;
+    private readonly token;
+    private readonly retryDelaysMs;
+    private readonly sleep;
+    constructor(token: string, retryDelaysMs?: number[], sleep?: (ms: number) => Promise<void>);
+    fetchReadme(projectUrl: string): Promise<string | null>;
+    private parseProjectUrl;
 }
 //# sourceMappingURL=GraphqlProjectRepository.d.ts.map
